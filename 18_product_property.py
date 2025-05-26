918ece0d9a5b0c976d2fd646b8b768057d3fbbcd
Hassan Nawaz <hassanniazi9118@gmail.com>
2025-05-27 03:17:11 +0500

    Assigment Completed

---
+++ 18_product_property.py
@@ -0,0 +1,22 @@
+class Product:
+    def __init__(self, price):
+        self._price = price
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        self._price = value
+
+    @price.deleter
+    def price(self):
+        del self._price
+
+# Test
+p = Product(99)
+print(p.price)
+p.price = 250
+print(p.price)
+del p.price
